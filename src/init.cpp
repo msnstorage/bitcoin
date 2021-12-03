5ed04a2561d48743faf6a5f16342d3aa3ab5ddf4
master <shultslara@mail.ru>
2021-12-03 23:41:40 +0300

    Fix Masternode lite mode

--- src/init.cpp
+++ src/init.cpp
@@ -1879,7 +1879,7 @@ bool AppInitMain(InitInterfaces& interfaces)
 
     // ********************************************************* Step 11d: start masternode-ps-<smth> threads
 
-    if (fMasterNode)
+    if (!fLiteMode)
         threadGroup.create_thread(boost::bind(&ThreadCheckMasternode, boost::ref(*g_connman)));
 
     // ********************************************************* Step 12: start node
